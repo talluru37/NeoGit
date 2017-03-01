dc79de42d4530153f2684054792b3e76df0199bf
talluru37 <talluru37@gmail.com>
2017-03-01 17:35:31 +0800

    release:perform

---
+++ ecm/src/main/java/com/mits/auto/ecm/ReleaseDemo2.java
@@ -0,0 +1,15 @@
+package com.mits.auto.ecm;
+
+public class ReleaseDemo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Release-2 Demo");
+
+	}
+	public String getDetails()
+	{
+		System.out.println("from getdetails..");
+		return "details";
+	}
+
+}
