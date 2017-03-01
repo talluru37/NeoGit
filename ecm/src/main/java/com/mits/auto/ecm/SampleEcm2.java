6dcf813424714da88787a49b19523f2e89434dfb
talluru37 <talluru37@gmail.com>
2017-03-01 15:06:16 +0800

    added 2nd file

---
+++ ecm/src/main/java/com/mits/auto/ecm/SampleEcm2.java
@@ -0,0 +1,16 @@
+package com.mits.auto.ecm;
+
+public class SampleEcm2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void setStatus()
+	{
+		System.out.println("set status");
+	}
+	
+
+}
