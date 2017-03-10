ff54efb7100f0252e784d3a00e9d33f8021e53bf
talluru37 <talluru37@gmail.com>
2017-03-10 19:40:46 +0800

    junit added

---
+++ JenkinsWebDemo/src/com/mits/test/cases/JunitTestSuite.java
@@ -0,0 +1,14 @@
+package com.mits.test.cases;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//JUnit Suite Test
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({ 
+   JavaTest.class ,TestJunit2.class
+})
+
+public class JunitTestSuite {
+}
