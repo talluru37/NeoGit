4ec037cb7aab7f12c4fae95a714185888a498175
talluru37 <talluru37@gmail.com>
2017-03-10 18:56:24 +0800

    getresults..

--- JenkinsWebDemo/src/com/mits/test/HelloWorld.java
+++ JenkinsWebDemo/src/com/mits/test/HelloWorld.java
@@ -4,5 +4,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("inside main...");
 	}
+	
+	public int getResults(int a,int b)
+	{
+		return a +b;
+	}
 
 }
